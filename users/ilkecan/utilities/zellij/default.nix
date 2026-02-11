66618f3def1429d59ae32ead34382935d2ea3628
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-11 23:24:10 +0300

    feat(home.ilkecan.zsh.auto_zellij): support layout names

--- users/ilkecan/utilities/zellij/default.nix
+++ users/ilkecan/utilities/zellij/default.nix
@@ -37,8 +37,12 @@ in
 
     zsh.initContent = ''
       auto_zellij() {
-        if [[ -z "$ZELLIJ" && -f zellij-layout.kdl ]]; then
-          zellij --layout zellij-layout.kdl
+        if [[ -z "$ZELLIJ" ]]; then
+          if [[ -f zellij-layout-name.txt ]]; then
+            zellij --layout $(cat zellij-layout-name.txt)
+          elif [[ -f zellij-layout.kdl ]]; then
+            zellij --layout zellij-layout.kdl
+          fi
         fi
       }
 
