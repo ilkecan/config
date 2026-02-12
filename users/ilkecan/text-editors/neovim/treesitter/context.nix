802155365988973fddc8a230559c35f2c4029874
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-12 13:16:14 +0300

    nvim: add keybinding for `TSContext "toggle"`

---
+++ users/ilkecan/text-editors/neovim/treesitter/context.nix
@@ -0,0 +1,24 @@
+# https://github.com/nvim-treesitter/nvim-treesitter-context
+{
+  ...
+}:
+
+{
+  programs.nvf.settings.vim = {
+    treesitter.context = {
+      enable = true;
+      setupOpts = {
+      };
+    };
+
+    keymaps = [
+      {
+        desc = "Toggle TS context";
+        key = "<F3>";
+        mode = [ "n" ];
+        action = ''function() vim.cmd.TSContext("toggle") end'';
+        lua = true;
+      }
+    ];
+  };
+}
