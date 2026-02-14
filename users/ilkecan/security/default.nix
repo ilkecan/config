321f6d0792404b7cd25f58ae303bb2902c25a4dd
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-14 15:53:24 +0300

    packages: add `bubblewrap`

--- users/ilkecan/security/default.nix
+++ users/ilkecan/security/default.nix
@@ -11,6 +11,7 @@
 
   home.packages = with pkgs; [
     age
+    bubblewrap # https://github.com/containers/bubblewrap
     gitleaks  # https://github.com/gitleaks/gitleaks
     sops
     ssh-to-age
