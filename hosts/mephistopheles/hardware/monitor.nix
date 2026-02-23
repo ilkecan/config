3d528b54e8d18664084da45c778e1be8852fdf42
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-23 10:35:52 +0300

    kernel: add `ddcci-backlight` module

--- hosts/mephistopheles/hardware/monitor.nix
+++ hosts/mephistopheles/hardware/monitor.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   ...
 }:
@@ -8,4 +9,16 @@
     ddcui
     ddcutil
   ];
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+  boot.kernelModules = [ "ddcci-backlight" ];
+
+  services.udev.extraRules = let
+    # https://wiki.nixos.org/wiki/Backlight#Via_ddcci-driver
+    bash = "${pkgs.bash}/bin/bash";
+    ddcciDev = "AUX USBC3/DDI TC3/PHY TC3";
+    ddcciNode = "/sys/bus/i2c/devices/i2c-14/new_device";
+  in ''
+    SUBSYSTEM=="i2c", ACTION=="add", ATTR{name}=="${ddcciDev}", RUN+="${bash} -c 'sleep 30; printf ddcci\ 0x37 > ${ddcciNode}'"
+  '';
 }
