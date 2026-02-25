9fd9b90eeac11eccd61c661d296dd0441850b107
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-26 01:51:10 +0300

    lib: define `importTree`

--- lib/my.nix
+++ lib/my.nix
@@ -1,18 +1,28 @@
 {
+  lib,
   pkgs,
   ...
 }:
 
 let
-  inherit (pkgs)
-    lib
+  inherit (builtins)
+    readDir
   ;
 
   inherit (lib)
     functionArgs
     intersectAttrs
     isFunction
+    isPath
+    mapAttrs
   ;
+
+  inherit (lib.my)
+    importTree
+    relativeTo
+  ;
+
+  INFINITY = 1.0e308 * 2;
 in
 {
   callExpression =
@@ -27,6 +37,33 @@ in
     in
     callExpressionWith pkgs;
 
-  relativeTo = dir: path:
-    dir + "/${toString path}";
+  importTree =
+    {
+      root,
+      depth ? INFINITY,
+      importFn ? import,
+    }:
+    let
+      importFile = path:
+        importFn (relativeTo root path);
+      importDir = path:
+        mapAttrs (name: type:
+          if type == "directory" then
+            importTree { root = relativeTo root name; depth = depth - 1; inherit importFn; }
+          else
+            importFile name
+        ) (readDir path);
+    in
+    if depth <= 0 then
+      importFile root
+    else
+      importDir root
+    ;
+
+  relativeTo = root: path:
+    if isPath path then
+      path
+    else
+      root + "/${toString path}"
+    ;
 }
