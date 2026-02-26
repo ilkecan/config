f804cbd2fc9985eb3fc73150519f81831143c883
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-26 15:41:58 +0300

    flake: define `mkHomeConfiguration`

--- flake/users/default.nix
+++ flake/users/default.nix
@@ -1,9 +1,54 @@
 {
+  self,
+  withSystem,
   ...
 }:
 
+let
+  mkHomeConfiguration =
+    {
+      system,
+      module,
+      osConfig ? null,
+    }:
+    let
+      module' =
+        {
+          inputs',
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        inputs'.home-manager.lib.homeManagerConfiguration {
+          inherit lib pkgs;
+
+          modules = [
+            inputs'.betterfox-nix.homeModules.betterfox
+            inputs'.dms-plugin-registry.modules.default
+            inputs'.dms.homeModules.dank-material-shell
+            inputs'.dms.homeModules.niri
+            inputs'.niri-flake.homeModules.niri
+            inputs'.niri-flake.homeModules.stylix
+            inputs'.nix-flatpak.homeManagerModules.nix-flatpak
+            inputs'.nix-index-database.homeModules.default
+            inputs'.nixcord.homeModules.nixcord
+            inputs'.nvf.homeManagerModules.default
+            inputs'.sops-nix.homeManagerModules.sops
+            inputs'.stylix.homeModules.stylix
+
+            module
+          ];
+
+          extraSpecialArgs = {
+            inherit inputs' self' osConfig;
+          };
+        };
+    in
+    withSystem system module';
+in
 {
-  imports = [
-    ./ilkecan.nix
-  ];
+  flake.homeConfigurations = {
+    ilkecan = mkHomeConfiguration { system = "x86_64-linux"; module = "${self}/users/ilkecan/home.nix"; osConfig = self.nixosConfigurations.mephistopheles.config; };
+  };
 }
