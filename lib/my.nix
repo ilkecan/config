b8267d926e28f0df09dea94b2943f724b61f6145
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-26 17:11:06 +0300

    lib: define `flattenAttrs`

--- lib/my.nix
+++ lib/my.nix
@@ -9,6 +9,7 @@ let
 
   inherit (lib)
     id
+    isAttrs
     isPath
     mapAttrs'
     nameValuePair
@@ -24,17 +25,32 @@ let
   INFINITY = 1.0e308 * 2;
 in
 {
+  flattenAttrs = sep: attrs:
+    let
+      flatten = prefix: attrs:
+        lib.foldlAttrs (acc: name: value:
+          let
+            name' = if prefix == "" then name else "${prefix}${sep}${name}";
+          in
+          if isAttrs value then
+            acc // flatten name' value
+          else
+            acc // { ${name'} = value; }
+        ) {} attrs;
+    in
+    flatten "" attrs;
+
   importTree =
     {
       root,
-      depth ? INFINITY,
-      normalizeNameFn ? id,
 
+      depth ? INFINITY,
       # NOTE: Prefer `importApply` over bare `import` when a module expression
       # is required. If there is no extra arguments to be passed, file path
       # (`importFn = id`) is also preferable to `import`.
       # https://noogle.dev/f/lib/modules/importApply
       importFn ? import,
+      normalizeNameFn ? id,
     }:
     let
       importFile = path:
