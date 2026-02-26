b277e0984614e11f8742fb37b1f81e97f738143e
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-26 15:41:36 +0300

    flake: define `mkNixosConfiguration`

--- flake/hosts/default.nix
+++ flake/hosts/default.nix
@@ -1,9 +1,48 @@
 {
+  self,
+  withSystem,
   ...
 }:
 
+let
+  mkNixosConfiguration =
+    {
+      system,
+      module,
+      homeConfig ? null,
+    }:
+    let
+      module' = {
+        inputs',
+        lib,
+        pkgs,
+        self',
+        ...
+      }:
+      inputs'.nixpkgs.lib.nixosSystem {
+        modules = [
+          inputs'.nixpkgs.nixosModules.readOnlyPkgs
+          { nixpkgs.pkgs = pkgs; }
+
+          inputs'.disko.nixosModules.disko
+          inputs'.impermanence.nixosModules.impermanence
+          inputs'.nix-gaming.nixosModules.pipewireLowLatency
+          inputs'.nix-gaming.nixosModules.platformOptimizations
+          inputs'.nix-gaming.nixosModules.wine
+          inputs'.sops-nix.nixosModules.sops
+
+          module
+        ];
+
+        specialArgs = {
+          inherit inputs' lib self' homeConfig;
+        };
+      };
+    in
+    withSystem system module';
+in
 {
-  imports = [
-    ./mephistopheles.nix
-  ];
+  flake.nixosConfigurations = {
+    mephistopheles = mkNixosConfiguration { system = "x86_64-linux"; module = "${self}/hosts/mephistopheles/configuration.nix"; homeConfig = self.homeConfigurations.ilkecan.config; };
+  };
 }
