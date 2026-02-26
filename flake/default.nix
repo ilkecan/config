b1ae7d93ba13b429cbc4975f7bf3a1c418308fd5
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-26 14:36:21 +0300

    flake: move all logic to `flake/default.nix`

--- flake/default.nix
+++ flake/default.nix
@@ -7,8 +7,18 @@
   imports = [
     inputs.flake-parts.flakeModules.modules
 
-    ./modules/flake/per-system-flake-inputs.nix
+    ./modules/flake/per-system-flake-inputs.nix   # dogfood
 
+    ./hosts
+    ./lib.nix
     ./modules
+    ./nixpkgs.nix
+    ./users
+  ];
+
+  debug = true;
+
+  systems = [
+    "x86_64-linux"
   ];
 }
