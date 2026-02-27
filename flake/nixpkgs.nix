d23d16efcf7286633f00a3c05252eb93fb3f01a5
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-27 14:43:44 +0300

    packages: move pkgs instantiating logic to default.nix

--- flake/nixpkgs.nix
+++ flake/nixpkgs.nix
@@ -4,45 +4,7 @@
 }:
 
 {
-  perSystem = { system, ... }: {
-    _module.args.pkgs = import inputs.nixpkgs {
-      localSystem = { inherit system; };
-
-      config = {
-        allowUnfree = true;
-        permittedInsecurePackages = [
-        ];
-      };
-
-      overlays = [
-        (final: _prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            localSystem = final.stdenv.hostPlatform;
-            inherit (final) config;
-          };
-        })
-
-        (final: _prev: {
-          patched = import inputs.nixpkgs-patched {
-            localSystem = final.stdenv.hostPlatform;
-            inherit (final) config;
-          };
-        })
-
-        # TODO: until `stable` nixpkgs is updated to v26.05
-        # inputs.nur.overlays.default
-        (final: _prev: {
-          nur = (inputs.nur.overlays.default final.unstable final.unstable).nur;
-        })
-
-        inputs.nix-alien.overlays.default
-
-        (final: _prev: {
-          notashelf = {
-            inherit (inputs.flint.packages.${final.stdenv.hostPlatform.system}) flint;
-          };
-        })
-      ];
-    };
+  perSystem = { system, self', ... }: {
+    _module.args.pkgs = import "${self'}/packages" { inherit inputs system; };
   };
 }
