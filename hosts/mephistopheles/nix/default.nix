87fb08f08abd2a8f43434439ce40e71cb6e8d09f
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-01 01:43:23 +0300

    nixos: add `cache.numtide.com` as a substituter

--- hosts/mephistopheles/nix/default.nix
+++ hosts/mephistopheles/nix/default.nix
@@ -39,6 +39,8 @@ in
       substituters = mkForce (imap1 (i: x: "${x}?priority=${toString i}") [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+
+        "https://cache.numtide.com"
         "https://nix-gaming.cachix.org"
 
         # NUR
@@ -48,6 +50,7 @@ in
 
       trusted-public-keys = [
         "ilkecan.cachix.org-1:hXb7Vo9EzaXiEb0elvG6Tt5TrP3zrcadyoX8c+lbeCY="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A="
