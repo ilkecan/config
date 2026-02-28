7dbf18e0b723360f29b7e92abe7827f673343f32
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-02-28 16:30:03 +0300

    hm.packages: add `deadnix`

--- users/ilkecan/development/formatters.nix
+++ users/ilkecan/development/formatters.nix
@@ -5,6 +5,7 @@
 
 {
   home.packages = with pkgs; [
+    deadnix # https://github.com/astro/deadnix
     keep-sorted # https://github.com/google/keep-sorted
     nixfmt # https://github.com/NixOS/nixfmt
     treefmt # https://github.com/numtide/treefmt
