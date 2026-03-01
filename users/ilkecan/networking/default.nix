b38c0493f3ede27e6558ad652277f8a04c030814
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-01 15:24:46 +0300

    hm.packages: add `socat`

--- users/ilkecan/networking/default.nix
+++ users/ilkecan/networking/default.nix
@@ -14,13 +14,14 @@
     };
 
     packages = with pkgs; [
-      byedpi  # https://github.com/hufrea/byedpi
+      byedpi # https://github.com/hufrea/byedpi
       inetutils # for 'telnet'
-      ldns  # https://github.com/NLnetLabs/ldns, for `drill`
-      mtr   # https://github.com/traviscross/mtr
-      nmap  # https://github.com/nmap/nmap
-      proxychains-ng  # https://github.com/rofl0r/proxychains-ng
-      q   # https://github.com/natesales/q
+      ldns # https://github.com/NLnetLabs/ldns, for `drill`
+      mtr # https://github.com/traviscross/mtr
+      nmap # https://github.com/nmap/nmap
+      proxychains-ng # https://github.com/rofl0r/proxychains-ng
+      q # https://github.com/natesales/q
+      socat # required for `claude-code` sandboxing (`/sandbox`)
     ];
   };
 
