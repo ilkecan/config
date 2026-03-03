559aa0b23e038124ac4aad504a93a6a1bd3cb83e
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-04 01:10:05 +0300

    niri: don't hardcode the zellij layout in autostart
    ... commands

--- users/ilkecan/utilities/zellij/default.nix
+++ users/ilkecan/utilities/zellij/default.nix
@@ -1,7 +1,13 @@
 {
+  lib,
   ...
 }:
 
+let
+  inherit (lib)
+    mkOrder
+  ;
+in
 {
   imports = [
     ./keybindings.nix
@@ -25,23 +31,27 @@
       };
     };
 
-    zsh.initContent = ''
+    # before Powerlevel10k instant prompt
+    zsh.initContent = mkOrder 100 ''
       auto_zellij() {
         if [[ -z "$ZELLIJ" ]]; then
           file=".zellij-layout-name.txt"
           if [[ -f $file ]]; then
             zellij --layout $(cat $file)
+            return
           fi
 
           file=".zellij-layout.kdl"
           if [[ -f $file ]]; then
             zellij --layout $file
+            return
           fi
         fi
       }
 
       autoload -U add-zsh-hook
       add-zsh-hook chpwd auto_zellij
+      auto_zellij
     '';
   };
 }
--- users/ilkecan/wayland/niri/autostart.nix
+++ users/ilkecan/wayland/niri/autostart.nix
@@ -7,16 +7,18 @@ let
   inherit (config.home)
     homeDirectory
   ;
+
+  githubDir = "${homeDirectory}/repos/git/github.com";
 in
 {
   programs.niri.settings.spawn-at-startup = [
-    { argv = [ "alacritty" "--class" "alacritty-config" "--working-directory" "${homeDirectory}/repos/git/github.com/ilkecan/config" "--command" "zellij --layout .zellij-layout.kdl" ]; }
-    { argv = [ "alacritty" "--class" "alacritty-nixpkgs" "--working-directory" "${homeDirectory}/repos/git/github.com/NixOS/nixpkgs" "--command" "zellij --layout .zellij-layout.kdl" ]; }
-    { argv = [ "alacritty" "--class" "alacritty-review" "--working-directory" "${homeDirectory}/repos/git/github.com/NixOS/nixpkgs/review" "--command" "zellij --layout .zellij-layout.kdl" ]; }
+    { argv = [ "alacritty" "--class" "alacritty-config" "--working-directory" "${githubDir}/ilkecan/config" ]; }
+    { argv = [ "alacritty" "--class" "alacritty-nixpkgs" "--working-directory" "${githubDir}/NixOS/nixpkgs" ]; }
+    { argv = [ "alacritty" "--class" "alacritty-review" "--working-directory" "${githubDir}/NixOS/nixpkgs/review" ]; }
     { argv = [ "firefox" ]; }
     { argv = [ "io.gitlab.news_flash.NewsFlash" ]; }
     { argv = [ "solaar" "--window=hide" ]; }
-    { argv = [ "steam" ]; }
+    { argv = [ "steam" "-silent" ]; }
     { argv = [ "wasistlos" ]; }
   ];
 }
