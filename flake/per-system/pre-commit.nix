7cd46305bfeb9b57193b10df965bf1d00205b994
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-04 10:43:47 +0000

    git-hooks: configure `flint` to check flake inputs

---
+++ flake/per-system/pre-commit.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  ...
+}:
+
+let
+  inherit (lib)
+    getExe
+  ;
+in
+{
+  perSystem = { pkgs, ... }: {
+    pre-commit.settings.package = pkgs.unstable.prek;
+    pre-commit.settings.hooks = {
+      flint = {
+        enable = true;
+        name = "flint";
+        description = "Check flake dependencies";
+        entry = "${getExe pkgs.notashelf.flint} --fail-if-multiple-versions";
+        files = "^flake\.(nix|lock)$";
+        pass_filenames = false;
+      };
+    };
+  };
+}
