f8b96a6e95913e85c9b3e24c71dfc0064f254527
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-05 19:31:46 +0300

    hm.packages: add more nix linters
    - flake-checker
    - nixpkgs-hammering
    - nixpkgs-lint
    - statix

--- users/ilkecan/development/static-program-analysis/nix.nix
+++ users/ilkecan/development/static-program-analysis/nix.nix
@@ -6,7 +6,13 @@
 {
   home.packages = with pkgs; [
     deadnix # https://github.com/astro/deadnix
+    flake-checker # https://github.com/DeterminateSystems/flake-checker
     nixf-diagnose # https://github.com/inclyc/nixf-diagnose
     nixfmt # https://github.com/NixOS/nixfmt
+    nixpkgs-hammering # https://github.com/jtojnar/nixpkgs-hammering
+    nixpkgs-lint # https://github.com/nix-community/nixpkgs-lint
+    notashelf.flint # https://github.com/NotAShelf/flint
+    statix # https://github.com/oppiliappan/statix
+    vulnix # https://github.com/nix-community/vulnix
   ];
 }
