323fd5fd7e2cb6de38deca3b645b14159b42f826
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-09 23:53:58 +0300

    hm: configure `mcp-servers`

--- inputs/home-manager.nix
+++ inputs/home-manager.nix
@@ -8,5 +8,11 @@
   src = inputs.home-manager;
   name = "home-manager";
   pulls = {
+    "8290" = "sha256-hSnOwFX04GQcOwlxPsDJxVV8b6FIFvuckCAm0fjfLvI=";
+    "8305" = "sha256-osWWAfqhc9X8QlGjSe4cnffj/ljB+KQGfSLT3o4Dd60=";
+    "8365" = "sha256-AKXVJmltOEiYu71gTb5yhlKTP21Ce0vuEogstWOAUa8=";
+    "8770" = "sha256-1GctHo6/qowbuguH335KDtn3nAgwn6+cGlebfuE2QiY=";
+    "8543" = "sha256-Swwm+ejwIKVI4tQy7ilPNwMoUlBejPm2NfMDrY+dJz0=";
+    "8783" = "sha256-W8Ufvufloq3gNT2zxegKQUHmi3KZMlcQCp9gEi0ZHuE=";
   };
 }
