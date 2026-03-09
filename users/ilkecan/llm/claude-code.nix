8cfe21d4bc91f78b67e1e4e22cb9e5fdfeeaa73c
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-09 22:39:14 +0300

    hm.programs: configure `claude-code`

---
+++ users/ilkecan/llm/claude-code.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home = {
+    sessionVariables = {
+      CLAUDE_CODE_DISABLE_AUTO_MEMORY = 0;
+    };
+
+    packages = with pkgs; [ starship ];
+  };
+
+  programs = {
+    claude-code = {
+      enable = true;
+      package = pkgs.llm-agents.claude-code;
+    };
+  };
+}
