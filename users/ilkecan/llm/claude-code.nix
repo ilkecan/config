26e185de6eb3b5d310cda06116644c4a05badba3
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-10 16:30:31 +0300

    hm.mcp: actually enable `mcp-servers`

--- users/ilkecan/llm/claude-code.nix
+++ users/ilkecan/llm/claude-code.nix
@@ -15,7 +15,12 @@
   programs = {
     claude-code = {
       enable = true;
-      package = pkgs.llm-agents.claude-code;
+      package = (pkgs.llm-agents.claude-code.override {
+        disableTelemetry = true;
+      }).overrideAttrs (prev: {
+        # TODO: remove this after 26.05
+        meta = prev.meta // { maintainers = [ ]; }; # to fix `attribute 'ryoppippi' missing`
+      });
       enableMcpIntegration  = true;
     };
   };
