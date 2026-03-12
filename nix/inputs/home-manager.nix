d7bdd78eadf5ff6f7ef7ed51e2eda4b8ae1dc1b8
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-12 17:10:02 +0300

    hm.programs: configure codex

--- nix/inputs/home-manager.nix
+++ nix/inputs/home-manager.nix
@@ -21,9 +21,17 @@
       hash = "sha256-AKXVJmltOEiYu71gTb5yhlKTP21Ce0vuEogstWOAUa8=";
     } # claude-code: add rules support
     {
+      number = "8550";
+      hash = "sha256-qYqSF986WZmzVYFPii+Thf0fZqH7HT3JrglKrutxdwI=";
+    } # codex: add skills options
+    {
       number = "8543";
       hash = "sha256-Swwm+ejwIKVI4tQy7ilPNwMoUlBejPm2NfMDrY+dJz0=";
     } # claude-code: add enableMcpIntegration
+    {
+      number = "8712";
+      hash = "sha256-qA5TaU5tQuq4/UKCgCFfiLrjTva7LxdERnIWF413+l8=";
+    } # codex: add enableMcpIntegration option
     {
       number = "8770";
       hash = "sha256-1GctHo6/qowbuguH335KDtn3nAgwn6+cGlebfuE2QiY=";
@@ -32,5 +40,9 @@
       number = "8783";
       hash = "sha256-W8Ufvufloq3gNT2zxegKQUHmi3KZMlcQCp9gEi0ZHuE=";
     } # claude-code: add support for output styles
+    {
+      number = "8823";
+      hash = "sha256-P49hudgjkJOjeTElT7k5Yn6UWxStVamOU6oOZccPylI=";
+    } # codex: fix skills location for new version
   ];
 }
---
+++ nix/users/ilkecan/llm/codex.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home = {
+    sessionVariables = {
+    };
+
+    packages = with pkgs; [
+      nodejs_latest # https://github.com/nodejs/node - /experimental -> JavaScript REPL
+    ];
+  };
+
+  programs = {
+    codex = {
+      enable = true;
+      package = pkgs.llm-agents.codex;
+      enableMcpIntegration = true;
+      settings = {
+        model = "gpt-5.4";
+        model_reasoning_effort = "xhigh";
+        plan_mode_reasoning_effort = "xhigh";
+        personality = "pragmatic";
+
+        features = {
+          guardian_approval = true;
+          js_repl = true;
+          multi_agent = true;
+          use_linux_sandbox_bwrap = true;
+        };
+
+        tui = {
+          theme = "solarized-dark";
+          status_line = [
+            "model-with-reasoning"
+            "context-remaining"
+            "current-dir"
+
+            "project-root"
+            "git-branch"
+
+            "context-used"
+            "five-hour-limit"
+            "weekly-limit"
+
+            "context-window-size"
+            "used-tokens"
+            "total-input-tokens"
+            "total-output-tokens"
+          ];
+        };
+      };
+    };
+  };
+}
