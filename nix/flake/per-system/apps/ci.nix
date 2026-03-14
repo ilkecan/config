b6506395c161448ce6933e2850e7b1f2545e2d83
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-14 16:45:42 +0000

    nix/packages: create `patched/` directory

--- nix/flake/per-system/apps/ci.nix
+++ nix/flake/per-system/apps/ci.nix
@@ -5,7 +5,7 @@
 pkgs.writeShellApplication {
   name = "ci";
 
-  runtimeInputs = with pkgs; [ unstable.nix-fast-build ];
+  runtimeInputs = with pkgs; [ patched.nix-fast-build ];
 
   text = ''
     nix-fast-build --no-nom --skip-cached --cachix-cache ilkecan "$@"
