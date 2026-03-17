171b35cc7457d8fa7dcb94963c88d186aedeb234
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-17 13:10:47 +0300

    hm.nvim: add `yy` keymap to `oil.nvim`
    Copies the path relative to the git root, fallbacks to the filename.

--- nix/users/ilkecan/text-editors/neovim/plugins/oil-nvim.nix
+++ nix/users/ilkecan/text-editors/neovim/plugins/oil-nvim.nix
@@ -18,7 +18,43 @@ in
           skip_confirm_for_simple_edits = true;
           keymaps = {
             "y." = mkLuaInline ''
-              { "actions.copy_entry_path", mode = "n" };
+              { "actions.copy_entry_path", mode = "n" }
+            '';
+            "yy" = mkLuaInline ''
+              {
+                callback = function()
+                  local oil = require("oil")
+                  local actions = require("oil.actions")
+                  local entry = oil.get_cursor_entry()
+                  local dir = oil.get_current_dir()
+
+                  if not entry or not dir then
+                    return actions.copy_entry_filename.callback()
+                  end
+
+                  local git_dir = vim.fs.find(".git", {
+                    path = dir,
+                    upward = true,
+                  })[1]
+
+                  if not git_dir then
+                    return actions.copy_entry_filename.callback()
+                  end
+
+                  local relative_path = vim.fs.relpath(
+                    vim.fs.dirname(git_dir),
+                    dir .. entry.name
+                  )
+
+                  if not relative_path then
+                    return actions.copy_entry_filename.callback()
+                  end
+
+                  vim.fn.setreg(vim.v.register, relative_path)
+                end,
+                desc = "Copy path relative to git root, or filename",
+                mode = "n",
+              }
             '';
           };
         };
