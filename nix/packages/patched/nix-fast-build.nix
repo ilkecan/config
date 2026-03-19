1f35e84f5ac8fdbc6613f31846b8d4d431c64d81
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-19 18:31:42 +0300

    packages.patched.nix-fast-build: update

--- nix/packages/patched/nix-fast-build.nix
+++ nix/packages/patched/nix-fast-build.nix
@@ -3,18 +3,19 @@
   nix-fast-build,
 }:
 
-nix-fast-build.overrideAttrs (prev: {
-  version = "${prev.version}-unstable-2026-03-11";
-  src = prev.src.override {
-    tag = null;
-    rev = "f5ca03a4387aecd739d60c77ae26e567481c3013";
-    hash = "sha256-mInYAyDT49w0ux7Jgg4Ny3igyEKlRvqzG2KAG9YVmsg=";
-  };
-  patches = (prev.patches or [ ]) ++ [
-    (fetchpatch2 {
-      name = "support-impure-derivations.patch";
-      url = "https://github.com/Mic92/nix-fast-build/pull/301.diff?full_index=1";
-      hash = "sha256-xVN5nOUIDTrCJ6jBeKp/MiYkHULYvj0xgBFqiQ34f+U=";
-    })
-  ];
-})
+nix-fast-build.overrideAttrs (
+  _final: prev: {
+    version = "1.4.0-unstable-2026-03-19";
+    src = prev.src.override {
+      tag = "1.4.0";
+      hash = "sha256-sH/KWX8NO8iurnnkI7w8eWMkbnRBbvEIK9IW4LnR0qQ=";
+    };
+    patches = (prev.patches or [ ]) ++ [
+      (fetchpatch2 {
+        name = "guard-against-empty-outputs.patch";
+        url = "https://github.com/Mic92/nix-fast-build/pull/309.diff?full_index=1";
+        hash = "sha256-e+2AowxVyx2FbGxgZRND0PsR5gdLGXSAVTN9Ur0Ew4Y=";
+      })
+    ];
+  }
+)
