bd4bbbec926073a883d19c76fd1eeeec9104724a
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-03-30 19:31:15 +0300

    ilkecan/packages: add `zat`

--- nix/users/ilkecan/llm/default.nix
+++ nix/users/ilkecan/llm/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   ...
 }:
 
@@ -10,4 +11,8 @@ let
 in
 {
   imports = collectImports ./.;
+
+  home.packages = with pkgs; [
+    llm-agents.zat # https://github.com/bglgwyng/zat
+  ];
 }
