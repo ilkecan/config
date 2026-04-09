fdd310a0e67edef5adf8c4f37bc34649508be192
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-04-10 00:18:32 +0300

    ilkecan/services: configure `byedpi`

--- nix/users/ilkecan/networking/default.nix
+++ nix/users/ilkecan/networking/default.nix
@@ -30,6 +30,26 @@ in
   };
 
   services = {
+    byedpi = {
+      enable = true;
+      extraArgs = [
+        "-Ku"
+        "-a5"
+        "-An"
+        "-Kt"
+        "-V443"
+        "-o1"
+        "-An"
+        "-f-1"
+        "-t6"
+        "-T0.5"
+        "-Ars"
+        "-d0+sm"
+        "-t6"
+        "-At"
+        "-r1+s"
+      ];
+    };
     network-manager-applet.enable = true;
   };
 }
