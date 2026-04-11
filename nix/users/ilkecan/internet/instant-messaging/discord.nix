8ac7b90b71210d19c3df1f7d004c7c295a22d57b
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-04-11 23:57:08 +0300

    ilkecan/programs: configure `nixcord.dorion`

--- nix/users/ilkecan/internet/instant-messaging/discord.nix
+++ nix/users/ilkecan/internet/instant-messaging/discord.nix
@@ -1,46 +1,25 @@
 {
+  pkgs,
   ...
 }:
 
 {
-  # programs.nixcord = {
-  #   enable = true;
-  #   # quickCss = "some CSS";  # quickCSS file
-  #   config = {
-  #     useQuickCss = true;
-  #     themeLinks = [
-  #       "https://raw.githubusercontent.com/link/to/some/theme.css"
-  #     ];
-  #     frameless = true;                   # Set some Vencord/Equicord options
-  #     plugins = {
-  #       hideAttachments.enable = true;    # Enable a plugin (works with both Vencord and Equicord)
-  #       ignoreActivities = {              # Enable a plugin and set some options
-  #         enable = true;
-  #         ignorePlaying = true;
-  #         ignoreWatching = true;
-  #         ignoredActivities = [ "someActivity" ];
-  #       };
-  #     };
-  #   };
-  #   dorion = {
-  #     enable = true;
-  #     theme = "dark";
-  #     zoom = "1.1";
-  #     blur = "acrylic";       # "none", "blur", or "acrylic"
-  #     sysTray = true;
-  #     openOnStartup = true;
-  #     autoClearCache = true;
-  #     disableHardwareAccel = false;
-  #     rpcServer = true;
-  #     rpcProcessScanner = true;
-  #     pushToTalk = true;
-  #     pushToTalkKeys = ["RControl"];
-  #     desktopNotifications = true;
-  #     unreadBadge = true;
-  #   };
-  #   extraConfig = {
-  #     # Some extra JSON config here
-  #     # ...
-  #   };
-  # };
+  programs.nixcord = {
+    enable = true;
+    dorion = {
+      enable = true;
+      package = pkgs.unstable.dorion; # NOTE: building the default package fails with: substituteInPlace called without any files to operate on (files must come before options!)
+      cacheCss = true;
+      desktopNotifications = true;
+      proxyUri = "socks5://localhost:1080";
+      startMaximized = true;
+      sysTray = true;
+
+      extraSettings = {
+      };
+
+      keybinds = {
+      };
+    };
+  };
 }
