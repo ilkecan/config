41a802f730e499aade4c8bcb0d926eb22b6250f4
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-04-13 00:23:55 +0300

    ilkecan/gemini-cli: configure `settings`

--- nix/users/ilkecan/llm/gemini-cli.nix
+++ nix/users/ilkecan/llm/gemini-cli.nix
@@ -8,8 +8,35 @@
     gemini-cli = {
       enable = true;
       package = pkgs.llm-agents.gemini-cli;
-      # defaultModel = null;
       settings = {
+        security = {
+          auth = {
+            selectedType = "oauth-personal";
+          };
+          disableYoloMode = true;
+        };
+        ui = {
+          footer = {
+            showLabels = true;
+            items = [
+              "workspace"
+              "git-branch"
+              "sandbox"
+              "model-name"
+              "quota"
+              "context-used"
+            ];
+          };
+        };
+        general = {
+          vimMode = true;
+          enableNotifications = true;
+        };
+        agents = {
+          browser = {
+            blockFileUploads = true;
+          };
+        };
       };
     };
   };
