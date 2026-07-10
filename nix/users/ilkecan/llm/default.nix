7afab575c6a496e689d3086f94132e3833963b3f
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-10 17:48:05 +0300

    ilkecan/packages: add `copilot-cli`

--- nix/users/ilkecan/llm/default.nix
+++ nix/users/ilkecan/llm/default.nix
@@ -13,6 +13,7 @@ in
   imports = collectImports ./.;
 
   home.packages = with pkgs; [
+    llm-agents.copilot-cli # https://github.com/github/copilot-cli
     llm-agents.zat # https://github.com/bglgwyng/zat
   ];
 }
