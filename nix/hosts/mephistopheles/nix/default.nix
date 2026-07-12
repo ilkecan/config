2fbe12ac72f600bc6b7404db8572fe9d52f9b025
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-13 00:50:13 +0300

    mephi: don't define additional substituters system-wide
    ... and define them in `.envrc` to only make them available to `nh`
    commands within this repository. This is to avoid slowing down every
    other `nix build` on the system.

--- nix/hosts/mephistopheles/nix/default.nix
+++ nix/hosts/mephistopheles/nix/default.nix
@@ -7,12 +7,6 @@
 }:
 
 let
-
-  inherit (lib)
-    imap1
-    mkForce
-    ;
-
   inherit (lib.my)
     collectImports
     ;
@@ -36,28 +30,6 @@ in
         "nix-command"
       ];
       trusted-users = [ userConfig.home.username ];
-
-      substituters = mkForce (
-        imap1 (i: x: "${x}?priority=${toString i}") [
-          "https://cache.nixos.org"
-          "https://nix-community.cachix.org"
-
-          "https://cache.numtide.com"
-          "https://nix-gaming.cachix.org"
-
-          # NUR
-          "https://ilkecan.cachix.org"
-          "https://rycee.cachix.org"
-        ]
-      );
-
-      trusted-public-keys = [
-        "ilkecan.cachix.org-1:hXb7Vo9EzaXiEb0elvG6Tt5TrP3zrcadyoX8c+lbeCY="
-        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A="
-      ];
 
       auto-optimise-store = true;
       http-connections = 128; # default: 25
