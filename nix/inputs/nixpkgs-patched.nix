f4c00f4a674398b095447df5e9c24275227ca675
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-14 23:23:56 +0300

    flake: update inputs

--- nix/inputs/nixpkgs-patched.nix
+++ nix/inputs/nixpkgs-patched.nix
@@ -4,9 +4,5 @@
   input = "nixpkgs-unstable";
   name = "nixpkgs-patched";
   pulls = [
-    {
-      number = "480060";
-      hash = "sha256-vM0D/VZlA/MkWem1d3EDUqaTEwGHxwwmq2U+VMSuyNc=";
-    } # various: use absolute path in thumbnailer files
   ];
 }
