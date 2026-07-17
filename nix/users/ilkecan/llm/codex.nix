5905f1837c47b1f03b46df6e3867389416236554
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-17 22:00:08 +0300

    ilkecan/codex: update settings

--- nix/users/ilkecan/llm/codex.nix
+++ nix/users/ilkecan/llm/codex.nix
@@ -1,59 +1,24 @@
 {
-  lib,
   pkgs,
   ...
 }:
 
-let
-  inherit (lib)
-    getExe
-    ;
-in
 {
-  home = {
-    sessionVariables = {
-    };
-
-    packages = with pkgs; [
-      nodejs_latest # https://github.com/nodejs/node - /experimental -> JavaScript REPL
-    ];
-  };
-
   programs = {
     codex = {
       enable = true;
       package = pkgs.llm-agents.codex;
       # enableMcpIntegration = true; # disable until lazy loading is implemented https://github.com/openai/codex/issues/9266
       settings = {
-        model = "gpt-5.4";
+        model = "gpt-5.6-luna";
         model_reasoning_effort = "high";
         plan_mode_reasoning_effort = "xhigh";
         personality = "pragmatic";
 
         features = {
-          # guardian_approval = true; # to prevent wasting tokens (?)
           fast_mode = false;
-          js_repl = true;
           multi_agent = true;
-          use_linux_sandbox_bwrap = true;
         };
-
-        notify = [
-          (getExe (
-            pkgs.writeShellApplication {
-              name = "codex-notify";
-              runtimeInputs = with pkgs; [
-                jq
-                libnotify
-              ];
-              text = ''
-                thread_id=$(echo "$1" | jq -r '."thread-id"')
-                last_message=$(echo "$1" | jq -r '."last-assistant-message"')
-                notify-send "Codex - $thread_id" "$last_message"
-              '';
-            }
-          ))
-        ];
 
         tui = {
           status_line = [
