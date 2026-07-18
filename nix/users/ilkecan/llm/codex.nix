7e4a80fc827fadf1516890e7a4d352575f78f72c
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-18 14:57:22 +0300

    ilkecan/codex: add `PermissionRequest` hook

--- nix/users/ilkecan/llm/codex.nix
+++ nix/users/ilkecan/llm/codex.nix
@@ -1,14 +1,21 @@
 {
+  lib',
   pkgs,
   ...
 }:
 
+let
+  inherit (lib'.ilkecan)
+    writeNushellScript
+    ;
+in
 {
   programs = {
     codex = {
       enable = true;
       package = pkgs.llm-agents.codex;
       # enableMcpIntegration = true; # disable until lazy loading is implemented https://github.com/openai/codex/issues/9266
+
       settings = {
         model = "gpt-5.6-luna";
         model_reasoning_effort = "high";
@@ -19,6 +26,32 @@
           fast_mode = false;
           multi_agent = true;
         };
+
+        hooks.PermissionRequest = [
+          {
+            matcher = "*";
+            hooks = [
+              {
+                type = "command";
+                command = writeNushellScript "codex-permission-notify.nu" ''
+                  def main [] {
+                      let payload = (open /dev/stdin --raw | from json)
+
+                      let title = $"Codex - ($payload.hook_event_name)"
+                      let body = [
+                        $payload.tool_input.description,
+                        $payload.tool_input.command,
+                      ] | str join "\n"
+
+                      ^notify-send $title $body
+                  }
+                '';
+                timeout = 1;
+                statusMessage = "Sending notification";
+              }
+            ];
+          }
+        ];
 
         tui = {
           status_line = [
