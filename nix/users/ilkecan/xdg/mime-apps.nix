d4d90f24ce223eb36ae2a429ffadd98fc71e3090
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-22 20:06:29 +0300

    ilkecan/xdg: set `neovide` as default application

--- nix/users/ilkecan/xdg/mime-apps.nix
+++ nix/users/ilkecan/xdg/mime-apps.nix
@@ -8,13 +8,15 @@
   # https://wiki.archlinux.org/title/XDG_MIME_Applications
   xdg.mimeApps = {
     enable = true;
+
     defaultApplicationPackages = with pkgs; [
-      vlc
       config.programs.mpv.package
       config.programs.imv.package
       # config.programs.feh.package
       config.programs.firefox.package
       config.programs.zathura.package
+      neovide
+      vlc
     ];
   };
 }
