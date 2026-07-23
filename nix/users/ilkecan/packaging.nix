43ed0f6bd9a95fa4b3d08f9072a19e8921d73801
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-24 00:32:04 +0300

    ilkecan/packages: add `cntr`

--- nix/users/ilkecan/packaging.nix
+++ nix/users/ilkecan/packaging.nix
@@ -6,6 +6,7 @@
 {
   home.packages = with pkgs; [
     asar # https://github.com/electron/asar
+    cntr # https://github.com/Mic92/cntr, used by `pkgs.breakpointHook`
     dpkg # for `dpkg-deb`
     icoutils # for `icotool`
   ];
