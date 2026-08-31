21436b1ccfc4572a54e4d66f6891a9dffd6e019c
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-08-31 23:34:46 +0300

    ilkecan/omp: update `bash.patterns`

--- nix/users/ilkecan/llm/omp.nix
+++ nix/users/ilkecan/llm/omp.nix
@@ -15,16 +15,36 @@ in
     package = pkgs.llm-agents.omp; # https://github.com/can1357/oh-my-pi
     settings = {
       bash.patterns = map mkAllow [
+        "git branch *"
+        "git cat-file *"
         "git diff *"
+        "git for-each-ref *"
+        "git grep *"
         "git log *"
         "git ls-files *"
+        "git remote *"
+        "git rev-parse *"
         "git show *"
         "git status *"
+        "git tag --list"
+        "jq *"
+        "nix derivation show *"
         "nix eval *"
+        "nix flake check *"
+        "nix flake check"
+        "nix flake metadata *"
+        "nix flake metadata"
+        "nix flake show *"
+        "nix flake show"
+        "nix hash *"
+        "nix-prefetch-url *"
+        "nixfmt *"
         "node --check *"
         "node --test *"
+        "stat *"
+        "wc *"
+        "which *"
       ];
-      tools.approvalMode = "write";
     };
   };
 }
