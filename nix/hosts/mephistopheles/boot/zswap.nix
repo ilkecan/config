33c5c896161a7dcda3854384bf964dcb46ad281b
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-09-04 19:26:45 +0300

    mephi: use `boot.zswap` module
    Use the default `maxPoolPercent`, which is lower than before by 5%.

--- nix/hosts/mephistopheles/boot/zswap.nix
+++ nix/hosts/mephistopheles/boot/zswap.nix
@@ -3,10 +3,8 @@
 }:
 
 {
-  boot.kernelParams = [
-    "zswap.compressor=lz4" # compression algorithm
-    "zswap.enabled=1" # enables zswap
-    "zswap.max_pool_percent=30" # maximum percentage of RAM that zswap is allowed to use
-    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
-  ];
+  boot.zswap = {
+    enable = true;
+    maxPoolPercent = 25;
+  };
 }
